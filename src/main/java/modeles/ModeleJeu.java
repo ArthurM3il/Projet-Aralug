eae22d032e4b0f64aa812544de763f423cc63c50
Lucas Boyer <boyerl@3il.fr>
2024-03-27 22:17:16 +0100

    Correction modeles

--- src/main/java/modeles/ModeleJeu.java
+++ src/main/java/modeles/ModeleJeu.java
@@ -1,5 +1,17 @@
 package modeles;
 
-public class ModeleJeu
-{
+public class ModeleJeu {
+    private String texte;
+
+    public ModeleJeu(String texte) {
+        this.texte = texte;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
 }
--- src/main/java/modeles/ModeleScore.java
+++ src/main/java/modeles/ModeleScore.java
@@ -1,4 +1,17 @@
 package modeles;
 
 public class ModeleScore {
+    private String texte;
+
+    public ModeleScore(String texte) {
+        this.texte = texte;
+    }
+
+    public String getTexte() {
+        return texte;
+    }
+
+    public void setTexte(String texte) {
+        this.texte = texte;
+    }
 }
